1897a9dca54ab50896f4cb5bd7a95be1730c5011
jberthomieux <jberthomieux@yahoo.fr>
2020-04-09 09:38:18 -0400

    seventh

--- src/main/java/com/jackson/controller/JacksonController.java
+++ src/main/java/com/jackson/controller/JacksonController.java
@@ -1,12 +1,18 @@
 package com.jackson.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class JacksonController {
 	@GetMapping("/")
 	public String getMesssage(){
-		return "Welcome to SprBootGitJenkinsIntegration";
+		return "Welcome to GitAWSCodeDeploy";
+	}
+	@GetMapping("/aws")
+	public String getAWS() {
+		return "Welcome to AWS";
 	}
 }
